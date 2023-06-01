55a9ad9246b0a1de86dfa536786ff7b8ec8e795c
Matej Topolovac <>
2023-06-01 16:57:19 +0200

    added application struct for dependency injection pattern

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,13 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 
@@ -13,14 +19,29 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
 
-	log.Printf("Starting server on :%s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	infoLog.Printf("Starting server on :%s", *addr)
+
+	server := &http.Server{
+		Addr:     *addr,
+		Handler:  mux,
+		ErrorLog: errorLog,
+	}
+
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
 }
