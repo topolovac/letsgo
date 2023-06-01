bf72452f6fe2d0751bb333473813e102290b45a0
Matej Topolovac <>
2023-06-01 17:16:29 +0200

    created routes file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,32 +13,22 @@ type application struct {
 }
 
 func main() {
-
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
-	mux := http.NewServeMux()
-
 	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR:\t", log.Ldate|log.Ltime|log.Lshortfile)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
 	}
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	infoLog.Printf("Starting server on :%s", *addr)
 
 	server := &http.Server{
 		Addr:     *addr,
-		Handler:  mux,
+		Handler:  app.routes(),
 		ErrorLog: errorLog,
 	}
 
