cbc53eef0a9120631e6ba4d7a7fd3a58a0298339
Matej Topolovac <>
2023-06-12 13:47:11 +0200

    added authenticated middleware to add isAuthenticad flag to context

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -69,5 +69,10 @@ func (app *application) newTemplateData(r *http.Request) *TemplateData {
 }
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserID")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
 }
