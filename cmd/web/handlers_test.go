a589c708eb6f6753c514954be486e7746ba001b2
Matej Topolovac <>
2023-06-13 15:03:59 +0200

    refactored ping tests and created utils file

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"snippedbox.matejtop.com/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	app := newTestApplication(t)
 
-	ping(rr, r)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	res := rr.Result()
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, res.StatusCode, http.StatusOK)
-
-	defer res.Body.Close()
-
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, code, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
